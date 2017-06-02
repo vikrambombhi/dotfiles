7e3274f0bfb9497c4ab354153a14d6611e8e1988
Vikram Bombhi <vibombhi@genband.com>
2017-06-02 10:45:38 -0400

    created setup script thanks https://github.com/JackyChiu/dotfiles/blob/master/setup.sh

---
+++ setup.bash
@@ -0,0 +1,24 @@
+#/bin/bash
+
+# Varibles
+backup=~/dotfiles_backup
+dir=$(pwd)
+dotfiles=".vimrc .tmux.conf .vim"
+
+# Backup
+mkdir -p $backup
+echo "Backup dir: $backup"
+
+# Move backups and create sym links
+for dotfile in $dotfiles; do
+	mv ~/$dotfile $backup
+	echo "Current $dotfile moved to: $backup"
+	ln -s $dir/$dotfile ~/$dotfile
+	echo "Symbloic link for $dotfile made in ~"
+done
+
+# Dir for backup/swap files
+mkdir -p ~/.vim/.backup
+mkdir -p ~/.vim/.swap
+mkdir -p ~/.vim/.undo
+echo "Dirs made for backup/swap/undo files"
