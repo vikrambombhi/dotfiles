d3941570b18c4f0198d1da7d2c805072ef825a1a
vikrambombhi <vbombhi@gmail.com>
2018-10-24 13:11:05 -0400

    use ssh by default for git

--- setup.bash
+++ setup.bash
@@ -5,4 +5,7 @@ dir=$(pwd)
 
 echo "Copying neovim config"
 mkdir -p ~/.config/nvim
-ln -s $dir/nvim/init.vim ~/.config/nvim --backup=numbered
+ln -s $dir/nvim/init.vim ~/.config/nvim --backup=simple
+
+echo "Using ssh for git by default"
+ln -s $dir/.gitconfig ~/.gitconfig --backup=simple
