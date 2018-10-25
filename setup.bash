760b07565b56f00db50d43b0f142e9b1b27636e3
vikrambombhi <vbombhi@gmail.com>
2018-10-25 17:53:54 -0400

    use libinput for trackpad gestures

--- setup.bash
+++ setup.bash
@@ -7,5 +7,8 @@ echo "Copying neovim config"
 mkdir -p ~/.config/nvim
 ln -s $dir/nvim/init.vim ~/.config/nvim --backup=simple
 
+echo "Copy libinput config"
+ln -s $dir/libinput-gestures.conf ~/.config/libinput-gestures.conf --backup=simple
+
 echo "Using ssh for git by default"
 ln -s $dir/.gitconfig ~/.gitconfig --backup=simple
