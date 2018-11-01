99f1474cd0a1c992f92594b2491146142b06745d
vikrambombhi <vbombhi@gmail.com>
2018-11-01 16:37:02 -0400

    WIP install nvim, and libinput gestures on setup

--- setup.bash
+++ setup.bash
@@ -3,12 +3,26 @@
 # Varibles
 dir=$(pwd)
 
-echo "Copying neovim config"
-mkdir -p ~/.config/nvim
-ln -s $dir/nvim/init.vim ~/.config/nvim --backup=simple
+echo "Creating config folder"
+mkdir ~/.config
+echo "Creating dev folder using GOPATH structure"
+mkdir -p ~/dev/src/github.com
 
-echo "Copy libinput config"
+echo "Installing neovim"
+apt install neovim
+echo "Setting up neovim, dont forget to install packages after this"
+mkdir ~/.config/nvim
+ln -s $dir/nvim.vim ~/.config/nvim --backup=simple
+
+echo "Installing libintput-gestures for trackpad gestures"
+git clone https://github.com/bulletmark/libinput-gestures.git ~/dev/src/github.com/
+.~/dev/src/github.com/bulletmark/libinput-gestures-setup install
+echo "Setting up trackpad gestures"
 ln -s $dir/libinput-gestures.conf ~/.config/libinput-gestures.conf --backup=simple
+echo "Starting trackpad gestures service"
+libinput-gestures-setup autostart
+libinput-gestures-setup start
+echo "To stop geastures from autostarting use command 'libinput-gestures-setup autostop' "
 
-echo "Using ssh for git by default"
-ln -s $dir/.gitconfig ~/.gitconfig --backup=simple
+echo "Setting up git config"
+ln -s $dir/gitconfig ~/.gitconfig --backup=simple
