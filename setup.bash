7a3048937ad74eebd6f1d810ed884da99bf34803
Vikram Bombhi <vikram@Vikrams-MacBook-Pro-2.local>
2023-06-01 17:04:38 -0400

    Reworked entire setup script
    - dont install anything in setup script instead just exit early
    - only setup tmux and nvim, forget golang and libinput-geastures
    - only link config files for nvim if one doesnt already exist
    - only link config files for tmux if one doesnt already exist

--- setup.bash
+++ setup.bash
@@ -5,84 +5,47 @@ dir=$(pwd)
 
 # Create config and dev folders
 echo "Creating config folder"
-mkdir ~/.config
-echo "Creating dev folder using GOPATH structure"
-mkdir -p ~/dev/src/github.com
+mkdir -p ~/.config
+echo "Creating dev folder"
+mkdir -p ~/dev/
 
-# Allows you to cd into directory with directory name only (dont need to type `cd`)
-echo "" >> ~/.bashrc
-echo "# auto cd with dir name alone" >> ~/.bashrc
-echo "shopt -s autocd" >> ~/.bashrc
-echo "" >> ~/.bashrc
-
-# Install Golang if not already not installed
-if [ ! $(bash -c "command -v go") ]; then
-  echo "Golang not found installing it now"
-  sudo apt install -y golang-go
-
-  echo "" >> ~/.bashrc
-  echo "# Set GOPATH to custom path" >> ~/.bashrc
-  echo "export GOPATH=~/dev" >> ~/.bashrc
-
-  echo "# Add Go binaries to PATH" >> ~/.bashrc
-  echo "export PATH=\$PATH:\$GOPATH/bin" >> ~/.bashrc
-fi
 
 # Install tmux not already installed
 if [ ! $(bash -c "command -v tmux") ]; then
-  echo "Installing tmux"
-  sudo apt install -y tmux
-
-  # Set neovim config
-  echo "Setting up tmux"
-  ln -s $dir/tmux.conf ~/.tmux.conf --backup=simple
+	echo "Error: tmux should already be installed" >> /dev/stderr
+	exit
 fi
+
+if [ ! -f ~/.tmux/plugins/tpm/tpm ]; then
+	echo "Installing tpm"
+	git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+	echo "Remember to press prefix + I (capital i, as in Install) to install plugins"
+fi
+
+if [ ! -f ~/.tmux.conf ]; then
+	echo "Linking up tmux config"
+	ln -s $dir/tmux.conf ~/.tmux.conf
+fi
+
 
 # Install neovim not already installed
 if [ ! $(bash -c "command -v nvim") ]; then
-  # Install neovim
-  echo "Adding neovim PPA"
-  sudo apt-add-repository -y ppa:neovim-ppa/stable
-  sudo apt update
-  echo "Installing neovim"
-  sudo apt install -y neovim
-  echo "Installing python prerequisites for neovim"
-  sudo apt install -y python-dev python-pip python3-dev python3-pip
-
-  # Install vimplug
-  echo "Installing vimplug for neovim"
-  curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-
-  # Set neovim config
-  echo "Setting up neovim, dont forget to install packages after this"
-  mkdir ~/.config/nvim
-  ln -s $dir/nvim.vim ~/.config/nvim/init.vim --backup=simple
+	echo "Error: nvim should already be installed" >> /dev/stderr
+	exit
 fi
 
-# Install libintput-geastures for trackpad geastures
-if [ ! $(bash -c "command -v libinput-gestures") ]; then
-  # Install dependencies
-  sudo apt install -y xdotool wmctrl libinput-tools
+# Install vimplug
+echo "Installing vimplug for neovim"
+curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-  echo "Setting up trackpad gestures"
-  ln -s $dir/libinput-gestures.conf ~/.config/libinput-gestures.conf --backup=simple
-
-  echo "Download libintput-gestures for trackpad gestures"
-  go get github.com/bulletmark/libinput-gestures
-  cd /home/vikram/dev/src/github.com/bulletmark/libinput-gestures
-  sudo ./libinput-gestures-setup install
-
-  echo "Adding user to input group"
-  sudo gpasswd -a $USER input
-
-
-  echo "Starting trackpad gestures service"
-  libinput-gestures-setup autostart
-
-  echo "Restart computer for geastures to take effect"
-  echo "To stop geastures from autostarting use command 'libinput-gestures-setup autostop' "
+# Set neovim config
+if [ ! -f ~/.config/nvim/init.vim ]; then
+	echo "Linking neovim config, dont forget to install packages after this"
+	mkdir -p ~/.config/nvim
+	ln -s $dir/nvim/init.vim ~/.config/nvim/init.vim
 fi
 
-echo "Setting up git config"
-ln -s $dir/gitconfig ~/.gitconfig --backup=simple
+
+# Not sure if this is needed anymore
+# echo "Setting up git config"
+# ln -s $dir/gitconfig ~/.gitconfig --backup=simple
