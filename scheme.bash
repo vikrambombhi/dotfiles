796b7ca6add26dfb8dbae5c74540942fff8f568e
vikrambombhi <vbombhi@gmail.com>
2024-07-29 13:40:08 -0400

    setup by linking config dir only

--- scheme.bash
+++
@@ -1,4 +0,0 @@
-# Cool script to change terminal theme, checkout the devs site here
-# mayccoll.github.io/Gogh/
-
-bash -c  "$(wget -qO- https://git.io/vQgMr)" 
--- setup.bash
+++ setup.bash
@@ -4,15 +4,23 @@
 dir=$(pwd)
 
 # Create config and dev folders
-echo "Creating config folder"
-mkdir -p ~/.config
 echo "Creating dev folder"
 mkdir -p ~/dev/
 
-
-# Install tmux not already installed
+# Required dependencies
+# - tmux
+# - nvim
+# - ripgrep
 if [ ! $(bash -c "command -v tmux") ]; then
 	echo "Error: tmux should already be installed" >> /dev/stderr
+	exit
+fi
+if [ ! $(bash -c "command -v nvim") ]; then
+	echo "Error: nvim should already be installed" >> /dev/stderr
+	exit
+fi
+if [ ! $(bash -c "command -v rg") ]; then
+	echo "Error: ripgrep should already be installed" >> /dev/stderr
 	exit
 fi
 
@@ -22,35 +30,16 @@ if [ ! -f ~/.tmux/plugins/tpm/tpm ]; then
 	echo "Remember to press prefix + I (capital i, as in Install) to install plugins"
 fi
 
-if [ ! -f ~/.tmux.conf ]; then
-	echo "Linking up tmux config"
-	ln -s $dir/tmux.conf ~/.tmux.conf
-fi
 
-
-# Install neovim not already installed
-if [ ! $(bash -c "command -v nvim") ]; then
-	echo "Error: nvim should already be installed" >> /dev/stderr
-	exit
-fi
-
-# Install vimplug
-echo "Installing vimplug for neovim"
-curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-# Set neovim config
-if [ ! -f ~/.config/nvim/init.vim ]; then
-	echo "Linking neovim config, dont forget to install packages after this"
-	ln -s $dir/nvim/ ~/.config/nvim
+# Set configs
+if [ ! -f ~/.config ]; then
+	echo "Linking configs"
+	ln -s $dir/config/ ~/.config
 fi
 
 
 echo "Setting up git config"
 ln -s $dir/gitconfig ~/.gitconfig
-
-echo "Setting up alacrity"
-mkdir -p ~/.config/alacritty
-ln -s $dir/alacritty.yml ~/.config/alacritty/alacritty.yml
 
 if grep -Fxq "source $dir/dev.sh" "$HOME/.bashrc"; then
     echo "Dev util already installed"
