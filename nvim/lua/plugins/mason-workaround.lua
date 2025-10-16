5426bf5a41a27a5f3ee28b8f2b5861f339f20ee6
vikrambombhi <vbombhi@gmail.com>
2025-10-16 11:08:33 -0400

    no longer need mason workaround

--- nvim/lua/plugins/mason-workaround.lua
+++
@@ -1,6 +0,0 @@
--- Only needed while mason v2 is being supported by Lazy
--- https://github.com/LazyVim/LazyVim/issues/6039
-return {
-   { "mason-org/mason.nvim", version = "1.11.0" },
-   { "mason-org/mason-lspconfig.nvim", version = "1.32.0" },
-}
